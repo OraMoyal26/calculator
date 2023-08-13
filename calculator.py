52215a7433f7e5bb1c98947b748d7accf139b93c
Ora Moyal <oramoyal770@gmail.com>
2023-08-13 16:28:32 +0300

    The function can get string values

--- calculator.py
+++ calculator.py
@@ -1,15 +1,19 @@
 import math
 
 def plus(args):
+    args = [int(char) for char in args]
     return sum(args)
 
 def minus(args):
+    args = [int(char) for char in args]
     return args[0]-args[1]
 
 def multiply(args):
+    args = [int(char) for char in args]
     return math.prod(args)
 
 def divide(args):
+    args = [int(char) for char in args]
     if args[1]==0:
         raise ValueError("Division by zero is not allowed")
     return args[0]/args[1]
--- test_calculator.py
+++ test_calculator.py
@@ -2,20 +2,20 @@ import calculator as calculator
 import pytest
 
 def assert_fx_equals_y(function,args,ans):
-    assert function(args) == ans
+    assert function(args) == int(ans)
 
 def test_minus():
-    assert_fx_equals_y(calculator.minus,[6,2],4)
+    assert_fx_equals_y(calculator.minus,["6","2"],"4")
 
 def test_divide():
-    assert_fx_equals_y(calculator.divide,[6,2],3)
+    assert_fx_equals_y(calculator.divide,["6","2"],"3")
 
 def test_divide_by_zero():
     with pytest.raises(ValueError, match="Division by zero is not allowed"):
         calculator.divide([6,0])
 
 @pytest.mark.parametrize("plus_numbers, plus_expected_result", [
-    ([1, 2, 3], 6),
+    (["1", "2", "3"], "6"),
     ([2, 7, -10, -4], -5),
     ([-1, 0, 6, -8, 3], 0),
 ])
@@ -23,7 +23,7 @@ def test_plus(plus_numbers, plus_expected_result):
     assert_fx_equals_y(calculator.plus, plus_numbers, plus_expected_result)
 
 @pytest.mark.parametrize("multiply_numbers, multiply_expected_result", [
-    ([1, 2, 3], 6),
+    (["1", "2", "3"], "6"),
     ([2, 7, -10, -4], 560),
     ([-1, 3, 6, 2, 3], -108),
 ])
